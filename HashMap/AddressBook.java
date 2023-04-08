952d8d829cc13d9d932316740c954d25aa24e860
MuthyalaHarika <124594655+MuthyalaHarika@users.noreply.github.com>
2023-04-08 12:01:57 +0530

    Add files via upload

---
+++ HashMap/AddressBook.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class AddressBook
+  {
+    public static void main(String arg[])
+    {
+      HashMap<String,Integer,String,Integer> hm=new  HashMap<String,Integer,String,Integer>();
+      hm.put(new Integer("hhh",1-30,"bbbb",521208),"first object");
+      hm.put(new integer("ccc",1-89,"mmm",3216),"seconf object");
+      hm.put(new integer("kkk",1-70,"ooo",6789),"third object");
+       for (Map.Entry u: hm.entrySet())
+        {
+            System.out.println (u.getKey()+ "  " +u.getValue());
+        }
+    }
+
+    }
+class book
+  {
+    private String Name_of_person;
+    private int Building_no;
+    private String Strret_name;
+    private int pincode;
+    public book(String np,int bn,String sn,int p)
+    
+  }
+  
+    }
+  
+  
